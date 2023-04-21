0295b675c876955913ef5563f064837f625d12fe
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-21 23:15:11 +0530

    [Pankaj] Fibonacci Series

---
+++ LogicalPrograms/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Fibonacci
+    {
+        public static void FibonacciSeries()
+        {
+            int sum = 0;
+            int a= 0;
+            int b = 1;
+            for (int i = 0;  i< 10; i++)
+            {
+                sum = a + b ;
+                a = b;
+                b = sum;
+                
+                Console.WriteLine(sum);
+            }
+            
+        }
+
+    }
+}
